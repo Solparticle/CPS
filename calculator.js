a09714c64eb7e5a43d11079d204bc7f01501ee67
Solparticle <122699936+Solparticle@users.noreply.github.com>
2023-05-15 17:43:25 +0100

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,36 @@
+
+
+        let result = document.getElementById('result');
+
+        function appendChar(char) {
+
+            result.value += char;
+
+        }
+
+        function clearResult() {
+
+            result.value = '';
+
+        }
+
+        function deleteChar() {
+
+            result.value = result.value.slice(0, -1);
+
+        }
+
+        function calculateResult() {
+
+            try {
+
+                result.value = eval(result.value);
+
+            } catch (error) {
+
+                result.value = 'Error';
+
+            }
+
+        }
+
